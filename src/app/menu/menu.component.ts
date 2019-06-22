09ad0a25f03e3a21fbe8c9c9c0f4a0237f3cde37
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-06-22 11:55:40 +0300

    Add menu options and install bootstrap.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,6 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  items = [
+    {
+      name: 'Salad', choices: [
+        { name: 'Santa Fe' },
+        { name: 'Greek' },
+        { name: 'Asian' },
+      ],
+      related: [
+        {
+          name: 'Dressing', choices: [
+            { name: 'Italian' },
+            { name: 'Blue Cheese' },
+            { name: 'Ranch' },
+          ]
+        },
+
+        {
+          name: 'Bread', choices: [
+            { name: 'Italian' },
+            { name: 'Flat' },
+            { name: 'Sourdough' },
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Entree', choices: [
+        { name: 'Steak' },
+        { name: 'Salmon' },
+        { name: 'Rice' },
+      ],
+      related: [
+      ]
+    },
+    {
+      name: 'Soup', choices: [
+        { name: 'Minestrone' },
+        { name: 'Hot and sour' },
+        { name: 'Miso' },
+      ],
+      related: [
+        {
+          name: 'Bread', choices: [
+            { name: 'Breadsticks' }
+          ]
+        }
+      ]
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
